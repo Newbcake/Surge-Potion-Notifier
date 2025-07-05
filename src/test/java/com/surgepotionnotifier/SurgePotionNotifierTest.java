707e20702d64f9057441e3e92303ee1eda6fa6e9
Neal <9064744+Newbcake@users.noreply.github.com>
2025-07-05 21:21:24 +0200

    change test file

--- src/test/java/com/surgepotionnotifier/SurgePotionNotifierTest.java
+++ src/test/java/com/surgepotionnotifier/SurgePotionNotifierTest.java
@@ -3,7 +3,7 @@ package com.surgepotionnotifier;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class SurgePotionNotifierPluginTest
+public class SurgePotionNotifierTest
 {
 	public static void main(String[] args) throws Exception
 	{
